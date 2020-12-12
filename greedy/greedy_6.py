f88611af3c61b762bb90fd0f2e55b6d944548670
parkjieun <wldmsdl3187@naver.com>
2020-12-13 04:13:27 +0900

    greedy problem solved

---
+++ greedy/greedy_6.py
@@ -0,0 +1,15 @@
+# 단속카메라
+# https://programmers.co.kr/learn/courses/30/lessons/42884?language=python3#
+
+def solution(routes):
+    answer = 1
+    routes.sort(key=lambda x: (x[0], -x[1]))
+    end = routes[0][1]
+    for route in routes[1:]:
+        if route[0] <= end:
+            if end > route[1]:
+                end = route[1]
+            continue
+        answer += 1
+        end = route[1]
+    return answer
