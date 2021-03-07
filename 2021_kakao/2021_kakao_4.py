d5a12463078920d67b9534086531c50bacbd83e4
parkjieun <wldmsdl3187@naver.com>
2021-03-07 15:21:51 +0900

    2021_kakao problem solved

--- 2021_kakao/2021_kakao_4.py
+++ 2021_kakao/2021_kakao_4.py
@@ -1,46 +1,43 @@
-from collections import defaultdict
+# 합승 택시 요금
+# https://programmers.co.kr/learn/courses/30/lessons/72413
 
-def get_min_idx(visited, dist, n):
-    min_dist = 100000
-    min_idx = -1
-    for _n in range(1, n + 1):
-        if _n not in visited and dist[_n - 1] <= min_dist:
-            min_dist = dist[_n - 1]
-            min_idx = _n
-    return min_idx
+# 다익스트라 개념 잘 정리되어 있는 곳
+# https://mattlee.tistory.com/50
 
-def get_dijkstra(s, d, _fares, n):
-    visited = [s]
-    dist = init_dist(_fares[s], s, n)
-    s = get_min_idx(visited, dist, n)
-    while len(visited) != n:
-        visited.append(s)
-        queue = _fares[s].items()
-        for key, val in queue:
-            dist[key-1] = min(dist[key-1], dist[s-1] + val)
-        s = get_min_idx(visited,dist, n)
+import heapq
+
+def get_dijkstra(s, _fares):
+    queue = []
+    dist = {fare: float('inf') for fare in _fares}
+    dist[s] = 0
+    heapq.heappush(queue, [dist[s], s])
+    while queue:
+        cur_dist, cur_node = heapq.heappop(queue)
+        if dist[cur_node] < cur_dist:
+            continue
+        for new_node, new_dist in _fares[cur_node].items():
+            distance = cur_dist + new_dist
+            if distance < dist[new_node]:
+                dist[new_node] = distance
+                heapq.heappush(queue, [distance, new_node])
     return dist
 
-def init_dist(fare_s, s, n):
-    dist = [100000] * n
-    dist[s - 1] = 0
-    for key, val in fare_s.items():
-        dist[key - 1] = val
-    return dist
-
-def solution(n, s, a, b, fares):
-    answer = 100000000
+def init_fares(n, fares):
     _fares = dict()
-    for _n in range(1, n+1):
+    for _n in range(1, n + 1):
         _fares[_n] = {}
     for fare in fares:
         _fares[fare[0]].update({fare[1]: fare[-1]})
         _fares[fare[1]].update({fare[0]: fare[-1]})
+    return _fares
+
+def solution(n, s, a, b, fares):
+    answer = float('inf')
+    _fares = init_fares(n, fares)
+
     for _n in range(1, n+1):
-        answer = min(answer, get_dijkstra(s, _n, _fares, n)[_n-1] + get_dijkstra(_n, a, _fares,n)[a-1] + get_dijkstra( _n, b, _fares, n)[b-1])
+        both_dist = get_dijkstra(s, _fares)[_n]
+        each_dist = get_dijkstra(_n, _fares)
+        answer = min(answer, both_dist + each_dist[a] + each_dist[b])
+
     return answer
-
-
-
-
-solution(7, 3, 4, 1, [[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]])
