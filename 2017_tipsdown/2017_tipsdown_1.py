0de4b45b014afc51125fca13e4346babd5ce43fc
parkjieun <wldmsdl3187@naver.com>
2021-03-30 20:57:52 +0900

    2017_tipsdown_1.py modified

--- 2017_tipsdown/2017_tipsdown_1.py
+++ 2017_tipsdown/2017_tipsdown_1.py
@@ -2,22 +2,20 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12973
 
 ### 정확성 효율성 고치기
+
 def solution(s):
     if len(s) & 1 == 1:
         return 0
+    stack = []
+    for _s in s:
+        if not stack:
+            stack.append(_s)
+            continue
+        if stack[-1] == _s:
+            stack.pop()
+        else:
+            stack.append(_s)
 
-    while True:
-        if s[0] == s[1]:
-            s = s[2:]
-        else:
-            break
-    while True:
-        if s[-1] == s[-2]:
-            s = s[:-2]
-        else:
-            break
-    s1 = s[:len(s)//2]
-    s2 = s[len(s)//2:]
-    if s1[::-1] == s2:
+    if not stack:
         return 1
     return 0
