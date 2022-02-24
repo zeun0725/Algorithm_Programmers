53c6d1941f20e53dc0505c693c6fc2bd533386cf
parkjieun <wldmsdl3187@naver.com>
2022-02-24 23:41:18 +0900

    python algorithm book

--- python_algorithm_book/list2.py
+++ python_algorithm_book/list2.py
@@ -2,7 +2,20 @@
 from typing import List
 
 
+def up_point(height):
+    max_up_index = None
+    min_up_index = None
+
+def down_point(height, max_up_index, min_up_index):
+    return 0
+
+
 def solution(height: List[int]) -> int:
+    for i, h in enumerate(height[:-1]):
+        if height[i] > height[i + 1]:
+            pass
+
+
     pass
 
 
