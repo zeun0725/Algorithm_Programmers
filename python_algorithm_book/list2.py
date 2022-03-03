171a75c5ba2a3ec699ed90b4f771c08ba97300ef
parkjieun <wldmsdl3187@naver.com>
2022-03-03 22:43:20 +0900

    python algorithm book

--- python_algorithm_book/list2.py
+++ python_algorithm_book/list2.py
@@ -1,22 +1,48 @@
 # 빗물 트래핑
 from typing import List
+"""
+투포인터 최대 이동 => O(n) 에 풀이 가능
+최대 지점에서 좌우 포인터가 만나게 됨
+"""
+
+def trap(height: List[int]) -> int:
+    if not height:
+        return 0
+    volume = 0
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+
+    while left < right:
+        left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+        if left_max <= right_max:
+            volume += left_max - height[left]
+            left += 1
+        else:
+            volume += right_max - height[right]
+            right -= 1
+    return volume
 
 
-def up_point(height):
-    max_up_index = None
-    min_up_index = None
+print(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
 
-def down_point(height, max_up_index, min_up_index):
-    return 0
+"""
+스택 쌓기 => O(n) 에 풀이 가능
+변곡점 만날 때 마다 pop 해서 비교
+"""
 
+def trap(height: List[int]) -> int:
+    stack = []
+    volume = 0
+    for i in range(len(height)):
+        while stack and height[i] > height[stack[-1]]:
+            top = stack.pop()
+            if not len(stack):
+                break
+            distance = i - stack[-1] -1
+            waters = min(height[i], height[stack[-1]]) - height[top]
 
-def solution(height: List[int]) -> int:
-    for i, h in enumerate(height[:-1]):
-        if height[i] > height[i + 1]:
-            pass
+            volume += distance * waters
+        stack.append(i)
+    return volume
 
-
-    pass
-
-
-print(solution([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+print(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
